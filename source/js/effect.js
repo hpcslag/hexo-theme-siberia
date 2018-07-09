e19e05d081014267317f3eaac3e905d1d10a7ee3
MacTaylor <cslag@hotmail.com.tw>
2018-07-09 23:35:52 +0800

    initialization

---
+++ source/js/effect.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    var movementStrength = 25;
+    var height = movementStrength / $(window).height();
+    var width = movementStrength / $(window).width();
+    $("body").mousemove(function(e){
+        var pageX = e.pageX - ($(window).width() / 2);
+        var pageY = e.pageY - ($(window).height() / 2);
+        var newvalueX = width * pageX * -1 - 25;
+        var newvalueY = height * pageY * -1 - 50;
+
+        if(window.outerWidth > 800)
+            $('#banner').css("background-position", newvalueX+"px     "+newvalueY+"px");
+    });
+});
